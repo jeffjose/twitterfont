129db0945399b489b0f163ffe3c14a5c17139ccd
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-08-11 17:57:58 -0700

    Initial add

---
+++ src/components/FontDisplay.svelte
@@ -0,0 +1,35 @@
+<script>
+export let name, src, color, text = "abc", style;
+
+import {onMount} from 'svelte';
+
+onMount(async () => {
+
+  console.log('onMount' + src);
+
+  var style = document.createElement('style');
+  style.appendChild(document.createTextNode(`
+  @font-face {
+    font-family: "${name}";
+    src: url('${src}');
+  }
+  
+  `));
+
+  document.head.appendChild(style);
+
+})
+</script>
+
+<style>
+  .wrapper { 
+    grid-column: 1/1;
+    grid-row: 1/1;
+  }
+
+</style>
+
+
+<div class="wrapper" style="font-family:{name}, 'Adobe Blank'; font-size: 50pt; color: {color}; align-self: baseline; {style}">
+  <div>{text}</div>
+</div>
